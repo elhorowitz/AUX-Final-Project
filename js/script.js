a2dae674de7113c0056ad21f4d0aaafe37308eee
elhorowitz <ericalhorowitz@gmail.com>
2014-06-05 19:29:36 -0400

    added services & info styling

--- js/script.js
+++ js/script.js
@@ -32,4 +32,24 @@
     e.preventDefault();
     showForm($(this));
   });
+
+  //====================================
+  //Function to change image on hover
+  var orangeColor = function (el) {
+    var source = el.find($("img")).attr("src").replace("blue", "orange");
+    el.find($("img")).attr("src", source);
+  };
+
+  var blueColor = function (el) {
+    var source = el.find($("img")).attr("src").replace("orange", "blue");
+    el.find($("img")).attr("src", source);
+  };
+
+  $(".services-info figure").hover(function(e) {
+    e.preventDefault();
+    orangeColor($(this));
+  }, function(e) {
+    e.preventDefault();
+    blueColor($(this));
+  });
 })();
